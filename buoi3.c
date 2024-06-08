2e4ae1e8f354b793b9aeed25dc637bf5cb70f952
Nguyễn Anh Khoa <119764945+Khoaak123zxc@users.noreply.github.com>
2024-06-08 21:07:41 +0700

    Add files via upload

---
+++ buoi3.c
@@ -0,0 +1,16 @@
+/*
+BTVN:
+BTVN1: Viết chương trinh nhập vào 1 số nguyên dương n, in ra true nếu là số hoàn hảo,
+false nếu không phải (7đ)
+BTVN2: Viết chương trình nhập vào 1 số nguyên dương n, in ra true nếu là số nguyên tố,
+false nếu không phái (3đ)
+ */
+
+/*
+
+ */
+#include<stdio.h> // standard input output
+int main(){
+    
+    
+}
